bcc5d197ffaab2b0c799c8242478b17119416591
Nicholas <littlenickster@gmail.com>
2020-03-10 13:49:53 -0700

    Added user validation to the login page

--- flutterApp/lib/loginPage.dart
+++ flutterApp/lib/loginPage.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutterApp/EvaluatorPG.dart';
-import 'package:flutterApp/menuWidgets.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 
 class LoginPage extends StatelessWidget {
@@ -27,10 +28,28 @@ class LoginForm extends StatefulWidget {
 }
 
 class LoginFormState extends State<LoginForm> {
+  Future<bool> fetchCredentials() async {
+    bool flag = false;
+    const url = 'https://projectworkflow.firebaseio.com/credentials.json';
+    final response = await http.get(url);
+    Map<String, dynamic> verify = json.decode(response.body);
+    if (verify['Username'] == _email) {
+      flag = true;
+    } else {
+      flag = false;
+    }
+    if (verify['Password'] == _password) {
+      flag = true;
+    } else {
+      flag = false;
+    }
+    return flag;
+  }
+
+  String name;
+  String pass;
   final alphanumeric = RegExp(r'^[a-zA-Z0-9]+$');
   final _formKey = GlobalKey<FormState>();
-  // final _mainKey = GlobalKey<FormState>();
-  // bool loggedIn = false;
   var _email, _password;
 
   @override
@@ -39,105 +58,96 @@ class LoginFormState extends State<LoginForm> {
 
     return Form(
       key: _formKey,
-
-  child: SingleChildScrollView(
-
-      child: Stack(children: <Widget>[
-        Container(
-            //child:
-            ),
-        Center(
-          //instead of hardcode 175, container can be infinity length but the widgets within can be centered to be center of the container.
-          //or use media query class
-          child: Container(
-            
-            padding: EdgeInsets.fromLTRB(
-                MediaQuery.of(context).size.width * 0.40,
-                MediaQuery.of(context).size.width * 0.10,
-                MediaQuery.of(context).size.width * 0.40,
-                MediaQuery.of(context).size.width * 0.10),
-            child: Column(children: <Widget>[
-//////////////////////////////////////Text Fields
-              new MyImageWidget(),
-
-              TextFormField(
-                autocorrect: false,
-                decoration: InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                  ),
-                  prefixIcon: Icon(
-                    Icons.mail,
-                    color: Colors.red,
-                    size: 24.0,
-                  ),
-                  hintText: 'Email',
-                ),
-                //Validation Section for Email Textfield
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return ('Error: Email is empty.');
-                  }
-
-                  if (!value.contains('@')) {
-                    return ('Please type in a valid email address');
-                  }
-                },
-                onSaved: (str) {
-                  _email = str;
-                },
+      child: SingleChildScrollView(
+        child: Stack(children: <Widget>[
+          Container(
+              //child:
               ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 25.0),
-                child: TextFormField(
-                  obscureText: true,
+          Center(
+            //instead of hardcode 175, container can be infinity length but the widgets within can be centered to be center of the container.
+            //or use media query class
+            child: Container(
+              padding: EdgeInsets.fromLTRB(
+                  MediaQuery.of(context).size.width * 0.40,
+                  MediaQuery.of(context).size.width * 0.10,
+                  MediaQuery.of(context).size.width * 0.40,
+                  MediaQuery.of(context).size.width * 0.10),
+              child: Column(children: <Widget>[
+//////////////////////////////////////Text Fields
+                new MyImageWidget(),
+
+                TextFormField(
                   autocorrect: false,
                   decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.lock_outline,
-                        color: Colors.red,
-                        size: 24.0,
-                      ),
-                      hintText: 'Password'),
-                  //Validation Section for Password Textfield
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.mail,
+                      color: Colors.red,
+                      size: 24.0,
+                    ),
+                    hintText: 'Email',
+                  ),
+                  //Validation Section for Email Textfield
                   validator: (value) {
-                    if ((value.isEmpty) |
-                        (value.length <= 7) |
-                        (!value
-                            .contains(new RegExp(r'[!@#$%^&*(),.?":{}|<>]')))) {
-                      return ('Password is invalid.');
+                    if (value.isEmpty) {
+                      return ('Error: Email is empty.');
                     }
-                  },
-                  onSaved: (str) {
-                    //Database shit goes here
-                    _password = str;
+
+                    if (!value.contains('@')) {
+                      return ('Please type in a valid email address');
+                    }
+                    _email = value;
                   },
                 ),
-              ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 25.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.lock_outline,
+                          color: Colors.red,
+                          size: 24.0,
+                        ),
+                        hintText: 'Password'),
+                    //Validation Section for Password Textfield
+                    validator: (value) {
+                      if ((value.isEmpty) |
+                          (value.length <= 7) |
+                          (!value.contains(
+                              new RegExp(r'[!@#$%^&*(),.?":{}|<>]')))) {
+                        return ('Password is invalid.');
+                      }
+                      _password = value;
+                    },
+                  ),
+                ),
 
 ////////////////////////////////Submission Button
-              RaisedButton(
-                onPressed: onPressed,
-                child: Text('LOG IN'),
-              ),
-
-              FlatButton(
-                onPressed: () {},
-                child: Text(
-                  "Forgot password?",
-                  
+                RaisedButton(
+                  onPressed: onPressed,
+                  child: Text('LOG IN'),
                 ),
-              ),
+
+                FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Forgot password?",
+                  ),
+                ),
 ////////////////////////////
 
 /////////////////////////////
-            ]),
+              ]),
+            ),
           ),
-        ),
-      ]),
+        ]),
       ),
     );
   }
@@ -147,14 +157,13 @@ class LoginFormState extends State<LoginForm> {
 
     if (form.validate()) {
       form.save();
-      setState(() {
-        //    loggedIn = true;
-        //got to landing page for jesus
-        Navigator.push(
-          context,
-          //Comment me out and uncomment line under.
-          MaterialPageRoute(builder: (context) => FirstRoute()),
-        );
+      setState(() async {
+        if (await fetchCredentials() == true) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => FirstRoute()),
+          );
+        }
       });
     }
   }
